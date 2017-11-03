84b0c1756bc4287ed05fe6d232396cd567760829
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-02 22:14:08 -0700

    Change Welcome Msg from paragraph to heading

--- templates/home.php
+++ templates/home.php
@@ -1,6 +1,6 @@
 <?php namespace PH7App; ?>
 
-<p>Welcome <span class="italic"><?= $fullname ?></span>!</p>
+<h3>Welcome <span class="italic"><?= $fullname ?></span>!</h3>
 
 <p>
     <label for="payment_link" class="bold">Your Stripe Payment Link (to share with anyone!):</label><br>
