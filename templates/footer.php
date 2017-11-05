136ef8aa6a3cf947b12e58d4ca212296087ad25f
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-04 18:02:42 -0700

    Update github project URL + Store Admin Email into env file

--- templates/footer.php
+++ templates/footer.php
@@ -24,10 +24,10 @@
       <div class="container">
           <div class="row">
               <div class="col l9 s12">
-                  Proud to be <a class="orange-text text-lighten-3" href="https://github.com/pH-7/MyDreamLife.eu">open source</a>
+                  Proud to be an <a class="orange-text text-lighten-3" href="https://github.com/pH-7/Link2Payment">open source</a> project
               </div>
               <div class="col l3 s12">
-                  <a class="orange-text text-lighten-3" rel="nofollow" href="mailto:hi@ph7.me?subject=Regarding Link2Payment">Contact</a>
+                  <a class="orange-text text-lighten-3" rel="nofollow" href="mailto:<?= getenv('ADMIN_EMAIL') ?>?subject=Regarding Link2Payment">Contact</a>
               </div>
           </div>
       </div>
