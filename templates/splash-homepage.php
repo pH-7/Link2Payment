18cbab2877d8fba7b160bddb60a3eb2b0a8ddbca
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-05 11:32:03 -0800

    Add new paragraph on homepage

--- templates/splash-homepage.php
+++ templates/splash-homepage.php
@@ -9,4 +9,9 @@
     <i class="large material-icons">cake</i>
 
     <i class="large material-icons">av_timer</i>
+
+    <p>
+        <span class="bold">"One Account" = "One Stripe Link Form" (to share anywhere with anyone)</span><br />
+        <span class="small">You will need more accounts (with other valid email addresses) if you need more stripe links.</span>
+    </p>
 </div>
