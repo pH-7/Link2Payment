ffd3d92dfb6823d5dd655162e4b53b7fcb12a437
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-07 10:57:25 -0800

    Cleanup error/not found page (and remove unused CSS class names

--- templates/error.php
+++ templates/error.php
@@ -1,16 +1,8 @@
 <?php namespace PH7App; ?>
 
-<div class="createAcc">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="panel">
-                    <h2>Error Occured!</h2>
-                    <p><strong>Oops! An error has occurred!</strong></p>
-                    <p><?= escape($message) ?></p>
-                    <p>Please try again later or <a href="mailto:<?= getenv('ADMIN_EMAIL') ?>?subject=Error with Link2Payment">contact us</a> if the problem persists.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<section class="center">
+    <h2>Error Occured!</h2>
+    <p><strong>Oops! An error has occurred!</strong></p>
+    <p><?= escape($message) ?></p>
+    <p>Please try again later or <a href="mailto:<?= getenv('ADMIN_EMAIL') ?>?subject=Error with Link2Payment">contact us</a> if the problem persists.</p>
+</section>
--- templates/not-found.php
+++ templates/not-found.php
@@ -1,20 +1,18 @@
 <?php namespace PH7App; ?>
 
-<div class="createAcc">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="panel">
-                    <h2>Page Not Found!</h2>
-                    <p><strong>Sorry, we weren't able to find the page you requested.</strong></p>
-                    <p><strong><em>Suggestions:</em></strong></p>
-                    <ul>
-                        <li><a href="<?= site_url(); ?>">Main Page</a></li>
-                        <li><a href="javascript:void(0);" onclick="history.go(-1);">Go back to the previous page</a>
-                        </li>
-                    </ul>
-                </div>
+<section class="center">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel">
+                <h2>Page Not Found!</h2>
+                <p><strong>Sorry, we weren't able to find the page you requested.</strong></p>
+                <p><strong><em>Suggestions:</em></strong></p>
+                <ul>
+                    <li><a href="<?= site_url(); ?>">Main Page</a></li>
+                    <li><a href="javascript:void(0);" onclick="history.go(-1);">Go back to the previous page</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-</div>
+</section>
