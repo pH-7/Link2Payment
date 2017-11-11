9af9db6e7490ac9ef5520287a840a49604e0ae8e
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-11 00:34:39 -0800

    Save project to GitLab too!

--- save-code.sh
+++ save-code.sh
@@ -3,6 +3,11 @@ git remote rm origin
 git remote add origin git@bitbucket.org:pH_7/Link2Payment.git
 git push
 
+# GitLab repo
+git remote rm origin
+git remote add origin git@gitlab.com:pH-7/Link2Payment.git
+git push
+
 # GitHub repo
 git remote rm origin
 git remote add origin git@github.com:pH-7/Link2Payment.git
