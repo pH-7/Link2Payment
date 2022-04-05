e71898744132499f380679180721606907dde739
Pierre-Henry Soria <hi@ph7.me>
2022-04-05 20:57:46 +0930

    Readability: Better HTML indentation

--- templates/payment-done.php
+++ templates/payment-done.php
@@ -1,5 +1,9 @@
 <?php namespace PH7App; ?>
 
-<p class="green-text bold">Payment Successfully <span class="underline">Done</span>!</p>
+<p class="green-text bold">
+  Payment Successfully <span class="underline">Done</span>!
+</p>
 
-<p class="italic">For any further information, please contact the buyer at <a href="mailto:<?= $buyer_email ?>"><?= $buyer_email ?></a></p>
+<p class="italic">
+  For any further information, please contact the buyer at <a href="mailto:<?= $buyer_email ?>"><?= $buyer_email ?></a>
+</p>
