9f3f01d8fec16662c49f455bbfe4ee61d10b558a
Pierre-Henry Soria <hi@ph7.me>
2022-04-09 22:00:07 +0930

    Add missing header copyright

--- app/config/environment.php
+++ app/config/environment.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2017-2022, Pierre-Henry Soria.
+ * @license        GNU General Public License
+ */
 
 namespace PH7App;
 
