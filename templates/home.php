15469ae16f3c69c16d9c6aa762cb297baa711955
Pierre-Henry Soria <hi@ph7.me>
2022-04-21 05:39:09 +0930

    Home: Shorter field label

--- templates/home.php
+++ templates/home.php
@@ -10,7 +10,7 @@
     <p>&nbsp;</p>
 
     <p>
-        <label for="payment_link" class="bold">Your Stripe Payment Link (to share with anyone!):</label><br>
+        <label for="payment_link" class="bold">Your Payment Link (to share with anyone!):</label><br>
         <input class="center" type="text" name="payment_link" value="<?= $payment_link ?>" id="payment_link" readonly onclick="this.select()">
     </p>
 </div>
